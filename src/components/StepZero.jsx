1791a360274e1b475c0f25ea71677cce0a7a494a
Janek <teojanekathryn@gmail.com>
2020-03-15 15:29:39 +0800

    Updated Step 0 page. Updated footer Re-order bootstrap in header loading.

--- src/components/StepZero.jsx
+++ src/components/StepZero.jsx
@@ -10,31 +10,60 @@ class StepZero extends Component {
 	}
 
 	render() {
-		const {values} = this.props;
+		const { values } = this.props;
 		return (
 			<Form>
-				<h1 className="ui centered">Are you experiencing any of the following:</h1>
-				<p>Use this self-assessment tool to help determine whether you should be tested for COVID-19. You can
-					complete this assessment for yourself or on behalf of someone else, if they are not able.</p>
-				<p>We strongly urge anyone who has symptoms, including a cough, runny nose, fever or sore throat, to
-					self-isolate for 14 days.</p>
-				<p>To protect yourself while out in public, wash your hands frequently, and maintain a distance of
-					about 2 metres from others.</p>
-				<ul>
-					<li>severe difficulty breathing (e.g., struggling for each breath, speaking in single words)</li>
-					<li>severe chest pain</li>
-					<li>having a very hard time waking up</li>
-					<li>feeling confused</li>
-					<li>lost consciousness</li>
-				</ul>
-				<Button onClick={this.nextStep}>Launch Self-Assessment</Button>
-				<p>Disclaimer: (Let's not get sued!!!) Fusce facilisis sapien eros, vitae efficitur risus auctor ac. Ut
-					scelerisque tortor
-					velit, eu tempor ligula egestas eget. In vehicula, massa eget fermentum ultricies, magna metus
-					convallis tortor, eu tempor augue diam maximus purus. Cras convallis, massa in dignissim semper, sem
-					dolor elementum leo, vel posuere orci nunc in ligula.</p>
-				<p>Please contact your local health authorities. We have a list of hospitals and LGUs that are here to
-					help answer your questions.</p>
+				<div className="col-10 offset-1">
+					<h1 className="text-center mt-5 mb-4">COVID-19 Self-Assessment</h1>
+					<br/>
+					<p>
+						Check if you need to be tested for COVID-19. You can also help others use this tool if they are not able
+					</p>
+					<p>
+						DOH strongly urges anyone who has symptoms, including a cough, runny nose, fever or sore throat, to self-isolate for 14 days.
+					</p>
+					<p>
+						To protect yourself while out in public, wash your hands frequently, and maintain a distance of about 2 metres from others.
+					</p>
+					<div className="my-5 text-center">
+						<Button onClick={this.nextStep} className="btn btn-primary btn-lg">Launch Self-Assessment</Button>
+						<p className="mt-3">This is also available in <a href="#">Tagalog</a>, more local dialects will be available soon.</p>
+					</div>
+					<p>
+						If you have no symptoms or exposure concerns, but have questions about COVID-19 or novel coronavirus, please visit the following resources:
+					</p>
+						<ul>
+							<li>
+								<a href="https://www.doh.gov.ph/2019-nCoV">https://www.doh.gov.ph/2019-nCoV</a> – Learn about COVID-19 and actions being taken to protect the health of Filipinos
+							</li>
+							<li>
+								Department of Health’s official social media pages
+								<ul>
+									<li>
+										<a href="https://www.facebook.com/OfficialDOHgov">https://www.facebook.com/OfficialDOHgov</a>
+									</li>
+									<li>
+										<a href="https://twitter.com/DOHgovph">https://twitter.com/DOHgovph</a>
+									</li>
+									<li>
+										<a href="http://bit.ly/DOHPhCOVID-19">Viber</a>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<a href="https://www.doh.gov.ph/2019-nCoV">https://www.doh.gov.ph/2019-nCoV</a> – Learn about COVID-19 and actions being taken to protect the health of Filipinos
+							</li>
+							<li>
+								<a href="#">Frequently Asked Questions</a>
+							</li>
+						</ul>
+					<br/>
+					<p><strong>Disclaimer:</strong> (Let's not get sued!!!) Fusce facilisis sapien eros, vitae efficitur risus auctor ac. Ut
+						scelerisque tortor
+						velit, eu tempor ligula egestas eget. In vehicula, massa eget fermentum ultricies, magna metus
+						convallis tortor, eu tempor augue diam maximus purus. Cras convallis, massa in dignissim semper, sem
+						dolor elementum leo, vel posuere orci nunc in ligula.</p>
+				</div>
 			</Form>
 		)
 	}
