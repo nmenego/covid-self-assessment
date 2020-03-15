fd33c094b6ee31f4722517a57de149de109dc00f
Nico Martin Eñego <nico.enego@transferwise.com>
2020-03-15 20:33:28 +0800

    add google analytics

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import React, {Component} from 'react';
 import './App.css';
 import MainForm from './components/MainForm';
 import {Container} from 'semantic-ui-react';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-160601011-1');
 
 class App extends Component {
 
 	render() {
+		ReactGA.pageview(window.location.pathname + window.location.search);
 		return (
 			<Container textAlign='center'>
 				<MainForm/>
