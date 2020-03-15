390c1d13e2f96523a7b1b2bee32c32c72b04b671
Nico Martin Eñego <nico.enego@transferwise.com>
2020-03-15 19:35:12 +0800

    copy fix

--- src/components/Emergency.jsx
+++ src/components/Emergency.jsx
@@ -20,7 +20,7 @@ class Emergency extends Component {
 				<h1 className="ui centered">Call your nearest emergency department.</h1>
 				<p>They will ask more questions to assess you further. You will be brought to the COVID-19 Isolation
 					Area for admission or the Resuscitation Area of the hospital if you don’t want to be admitted.</p>
-				<p>**links that direct them to the nearest testing centers</p>
+				<p>[This part is under construction, please contact DOH directly below.]</p>
 				<Button onClick={this.contactDetails} className="btn btn-primary btn-lg btn-block" role="button">COVID-19
 					Emergency Contact Details</Button>
 				<Button onClick={this.next} className="btn btn-primary btn-lg btn-block" role="button">Restart
