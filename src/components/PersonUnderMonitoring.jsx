7763a915754db356137d2f18788913dc5edb35d6
Janek <teojanekathryn@gmail.com>
2020-03-16 09:25:10 +0800

    Styling updates (#5)

--- src/components/PersonUnderMonitoring.jsx
+++ src/components/PersonUnderMonitoring.jsx
@@ -13,6 +13,7 @@ class PersonUnderMonitoring extends Component {
 			<div className="col-sm-12 col-md-10 offset-md-1">
 				<h1 className="text-center mt-5 mb-4">Since you don’t have any COVID-19 symptoms, you don’t need to be tested for
 					COVID-19.</h1>
+				<br/>
 				<p>However, there’s a chance you could get sick since it’s less than 14 days since your exposure. You
 					should self-isolate until 14 days after the exposure. Self-isolation means don’t go to any public
 					places, stay at home, and don’t have any visitors. Don’t share personal items like dishes, utensils
