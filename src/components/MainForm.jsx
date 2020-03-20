5648ea5f432e1844aad2460f7bd8eee8942f086f
Nico Martin Eñego <nico.enego@transferwise.com>
2020-03-20 21:01:11 +0800

    Revert "update yarn"
    This reverts commit 54506dc9a90039f348bcdf31fbef53bf78caf085.

--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -1,6 +1,5 @@
 // MainForm.jsx
 import React, {Component} from 'react';
-import { Switch } from 'react-router-dom';
 import StepZero from './StepZero';
 import StepOne from './StepOne';
 import StepTwo from './StepTwo';
@@ -130,81 +129,76 @@ class MainForm extends Component {
 	}
 
 	render() {
-		return (
-			<Switch>
-				
-			</Switch>
-		);
-		// // TODO: as of the moment this is REALLY messy. we need to fix handling of states and steps
-		// const {step} = this.state;
-		// const {hasSymptoms, hasTravel} = this.state;
-		// const values = {hasSymptoms, hasTravel};
-		// switch (step) {
-		// 	case 0:
-		// 		return <StepZero
-		// 			nextStep={this.nextStep}
-		// 			values={values}
-		// 			faqPage={this.faqPageStep}
-		// 			contactPage={this.contactPageStep}
-		// 		/>
-		// 	case 1:
-		// 		return <StepOne
-		// 			yesStep={this.exposureStep}
-		// 			noStep={this.travelHistoryStep}
-		// 			values={values}
-		// 		/>
-		// 	case 2: // 2.0 travel history step
-		// 		return <StepTwo
-		// 			yesStep={this.pumStep}
-		// 			noStep={this.noTestStep}
-		// 			back={this.prevStep}
-		// 			values={values}
-		// 		/>
-		// 	case 3: // 2.1 exposure step
-		// 		return <StepThree
-		// 			yesStep={this.exposureDurationStep}
-		// 			noStep={this.exposureDurationStep}
-		// 			back={this.prevStep}
-		// 			values={values}
-		// 		/>
-		// 	case 4: // 3.0 exposure duration step
-		// 		return <StepFour
-		// 			yesStep={this.emergencyStep}
-		// 			noStep={this.preexistingIllnessStep}
-		// 			back={this.prevStep}
-		// 			values={values}
-		// 		/>
-		// 	case 5: // 3.1 pre-existing illness
-		// 		return <StepFive
-		// 			yesStep={this.emergencyStep}
-		// 			noStep={this.closeContactStep}
-		// 			back={this.prevStep}
-		// 			values={values}
-		// 		/>
-		// 	case 6: // 3.2close-contact
-		// 		return <StepSix
-		// 			yesStep={this.emergencyStep}
-		// 			noStep={this.noTestWithSymptomsStep}
-		// 			back={this.prevStep}
-		// 			values={values}
-		// 		/>
-		// 	case 94:
-		// 		return <FAQPage next={this.initialState}/>
-		// 	case 95:
-		// 		return <NoTestWithSymptoms next={this.initialState}/>
-		// 	case 96:
-		// 		return <NoTest next={this.initialState}/>
-		// 	case 97:
-		// 		return <PersonUnderMonitoring next={this.initialState}/>
-		// 	case 98:
-		// 		return <ContactPage
-		// 			next={this.initialState}
-		// 		/>
-		// 	case 99:
-		// 		return <Emergency next={this.initialState} contactDetails={this.contactPageStep}/>
-		// 	default:
-		// 		return <Emergency next={this.initialState} contactDetails={this.contactPageStep}/>
-		// }
+		// TODO: as of the moment this is REALLY messy. we need to fix handling of states and steps
+		const {step} = this.state;
+		const {hasSymptoms, hasTravel} = this.state;
+		const values = {hasSymptoms, hasTravel};
+		switch (step) {
+			case 0:
+				return <StepZero
+					nextStep={this.nextStep}
+					values={values}
+					faqPage={this.faqPageStep}
+					contactPage={this.contactPageStep}
+				/>
+			case 1:
+				return <StepOne
+					yesStep={this.exposureStep}
+					noStep={this.travelHistoryStep}
+					values={values}
+				/>
+			case 2: // 2.0 travel history step
+				return <StepTwo
+					yesStep={this.pumStep}
+					noStep={this.noTestStep}
+					back={this.prevStep}
+					values={values}
+				/>
+			case 3: // 2.1 exposure step
+				return <StepThree
+					yesStep={this.exposureDurationStep}
+					noStep={this.exposureDurationStep}
+					back={this.prevStep}
+					values={values}
+				/>
+			case 4: // 3.0 exposure duration step
+				return <StepFour
+					yesStep={this.emergencyStep}
+					noStep={this.preexistingIllnessStep}
+					back={this.prevStep}
+					values={values}
+				/>
+			case 5: // 3.1 pre-existing illness
+				return <StepFive
+					yesStep={this.emergencyStep}
+					noStep={this.closeContactStep}
+					back={this.prevStep}
+					values={values}
+				/>
+			case 6: // 3.2close-contact
+				return <StepSix
+					yesStep={this.emergencyStep}
+					noStep={this.noTestWithSymptomsStep}
+					back={this.prevStep}
+					values={values}
+				/>
+			case 94:
+				return <FAQPage next={this.initialState}/>
+			case 95:
+				return <NoTestWithSymptoms next={this.initialState}/>
+			case 96:
+				return <NoTest next={this.initialState}/>
+			case 97:
+				return <PersonUnderMonitoring next={this.initialState}/>
+			case 98:
+				return <ContactPage
+					next={this.initialState}
+				/>
+			case 99:
+				return <Emergency next={this.initialState} contactDetails={this.contactPageStep}/>
+			default:
+				return <Emergency next={this.initialState} contactDetails={this.contactPageStep}/>
+		}
 	}
 }
 
