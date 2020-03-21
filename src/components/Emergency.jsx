fa1d2df6849b9af45dccc659a3566d3576a5f5d8
Nico Martin Eñego <nico.enego@transferwise.com>
2020-03-21 09:12:48 +0800

    copy fixes, deadlinks update

--- src/components/Emergency.jsx
+++ src/components/Emergency.jsx
@@ -13,17 +13,16 @@ class Emergency extends Component {
 				<h1 className="text-center mt-5 mb-4">Call your nearest emergency department.</h1>
 				<br/>
 				<p>
-					They will ask more questions to assess you further. You will be brought to the COVID-19 Isolation
-					Area for admission or the Resuscitation Area of the hospital if you don’t want to be admitted.
+					They will ask more questions to assess you further. Please call the hotlines first before heading to
+					ER to prevent further transmission to other people.
 				</p>
 				<p>
-					[This part is under construction, please contact DOH directly below.]
 					<li><a href="/contact" role="link">Hotline Directory</a></li>
 				</p>
 
 				<div className="my-5 text-center">
 					<Button onClick={this.next} className="btn btn-primary btn-lg" role="button">Restart
-					Assessment</Button>
+						Assessment</Button>
 				</div>
 			</div>
 		)
--- src/components/PersonUnderMonitoring.jsx
+++ src/components/PersonUnderMonitoring.jsx
@@ -10,18 +10,20 @@ class PersonUnderMonitoring extends Component {
 	render() {
 		return (
 			<div className="col-sm-12 col-md-10 offset-md-1">
-				<h1 className="text-center mt-5 mb-4">Since you don’t have any COVID-19 symptoms, you don’t need to be tested for
+				<h1 className="text-center mt-5 mb-4">Since you don’t have any COVID-19 symptoms, you don’t need to be
+					tested for
 					COVID-19.</h1>
 				<br/>
 				<p>However, there’s a chance you could get sick since it’s less than 14 days since your exposure. You
 					should self-isolate until 14 days after the exposure. Self-isolation means don’t go to any public
 					places, stay at home, and don’t have any visitors. Don’t share personal items like dishes, utensils
-					and towels. Wash your hands often. If you have questions, visit [link].</p>
+					and towels. Wash your hands often. If you have questions, visit the Department of Health website <a
+						href="https://www.doh.gov.ph/2019-nCov/infographics">infographics section</a>.</p>
 				<p>If you get any COVID-19 symptoms, take this self-assessment again. If you need information, go to
-					[link].</p>
+					our <a href="/faq">FAQs</a>.</p>
 				<div className="my-5 text-center">
 					<Button onClick={this.next} className="btn btn-primary btn-lg" role="button">Restart
-					Assessment</Button>
+						Assessment</Button>
 				</div>
 			</div>
 		)
